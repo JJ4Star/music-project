94691e45524b9ee6f544660413c2ccf44e5da74f
JoeWang <joewang@starlux-airlines.com>
2022-10-24 15:01:47 +0800

    add custom message & add alert

--- src/includes/validation.js
+++ src/includes/validation.js
@@ -3,6 +3,7 @@ import {
   Field as VeeField,
   defineRule,
   ErrorMessage,
+  configure,
 } from "vee-validate";
 import {
   required,
@@ -23,13 +24,44 @@ export default {
     app.component("ErrorMessage", ErrorMessage);
 
     defineRule("required", required);
+    defineRule("tos", required);
     defineRule("min", min);
     defineRule("max", max);
     defineRule("alpha_spaces", alphaSpaces);
     defineRule("email", email);
     defineRule("minVal", minVal);
     defineRule("maxVal", maxVal);
-    defineRule("confirmed", confirmed);
+    defineRule("passwords_mismatch", confirmed);
     defineRule("excluded", excluded);
+    defineRule("country_excluded", excluded);
+
+    // Custom Error Message
+    configure({
+      generateMessage: (ctx) => {
+        const messages = {
+          required: `The field ${ctx.field} is required.`,
+          min: `The field ${ctx.field} is too short.`,
+          max: `The field ${ctx.field} is too long.`,
+          alphaSpaces: `The field ${ctx.field} may only contain alphabetical characters and spaces.`,
+          email: `The field ${ctx.field} must be a valid email.`,
+          minVal: `The field ${ctx.field} is too low.`,
+          maxVal: `The field ${ctx.field} is too high.`,
+          excluded: `Your are not allowed to use this value for the field ${ctx.field}.`,
+          country_excluded: `Due to restrctions, we do not accept users from this location.`,
+          passwords_mismatch: "The passwords don't match",
+          tos: "You must accept the Terms of Service.",
+        };
+        
+        const message = messages[ctx.rule.name]
+          ? messages[ctx.rule.name]
+          : `The field ${context.field} is invalid.`;
+    
+          return message;
+      },
+      validateOnBlur: true, // If validation should be triggered on blur event, default is true
+      validateOnChange: true, // If validation should be triggered on change event, default is true
+      validateOnInput: false, // If validation should be triggered on input event, default is false
+      validateOnModelUpdate: true, // If validation should be triggered on update:modelValue (v-model) event, default is true
+    });
   },
 };
